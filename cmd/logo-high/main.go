addab657b3912aa0ce6986021bdb58b7cfd0ee01
Ole Bulbuk <ole@bulbuk.de>
2020-05-22 22:54:18 +0200

    Add high (almost quadratic) logo

--- cmd/logo-high/main.go
+++ cmd/logo-high/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	// global
-	oSize := 100
+	oSize := 300
 	margin := oSize / 10
 	color := "black"
 	stroke := oSize / 10
@@ -14,53 +14,50 @@ func main() {
 
 	// wing1
 	wing1ClipX := margin - stroke/2
-	wing1ClipY := margin - stroke/2
-	wing1ClipWidth := oSize + stroke/2
-	wing1ClipHeight := oSize + stroke/2
-	wing1CircX := wing1ClipX + wing1ClipWidth
-	wing1CircY := wing1ClipY + wing1ClipHeight
-	wing1CircR := oSize
+	wingClipY := margin + oSize
+	wingClipWidth := oSize + stroke/2 + margin
+	wingClipHeight := oSize + stroke/2
+	wing1CircX := wing1ClipX + wingClipWidth - margin
+	wingCircY := wingClipY + wingClipHeight
+	wingCircR := oSize
 
-	// O
+	// O (1)
 	oR := oSize / 2
-	oX := wing1ClipX + wing1ClipWidth + oR
 	oY := margin + oR
 
 	// ( )
 	parenR := oSize * 3 / 4
-	parenClipX := oX + oR + margin
-	parenClipY := oY - oR - stroke/2
+	parenClipX := wing1CircX
+	parenClipY := oY + oR + stroke/2
 	parenClipWidth := parenR*2 + stroke
 	parenClipHeight := oSize + stroke
 	parenX := parenClipX + parenR + stroke/2
-	parenY := oY
+	parenY := parenClipY + oR + stroke/2
+
+	// O (2)
+	oX := parenX
 
 	// n
+	nCircR := oSize / 3
 	nClipX := parenClipX + oSize*4/10 - stroke
 	nClipY := parenClipY
 	nClipWidth := oSize + stroke
-	nClipHeight := oSize/2 + stroke
-	nCircR := oSize / 3
+	nClipHeight := nCircR + stroke/2 + margin
 	nCircX := parenX
 	nCircY := nClipY + nClipHeight
 	nLine1X := nCircX - nCircR
-	nLine1Y1 := nClipY + nClipHeight - nCircR - stroke/2
-	nLineY1 := nClipY + nClipHeight
-	nLineY2 := nLineY1 + oSize/2
+	nLine1Y1 := nCircY - nCircR - stroke/2
 	nLine2X := nCircX + nCircR
+	nLine2Y1 := nCircY
+	nLineY2 := parenClipY + parenClipHeight - margin
 
 	// wing2
-	wing2X := parenClipX + parenClipWidth
-	wing2Y := margin
-	wing2ClipWidth := oSize + stroke
-	wing2ClipHeight := oSize*2 + stroke/2
-	wing2CircR := oSize
-	wing2ElliRX := wing2ClipWidth - stroke/2
-	wing2ElliRY := wing2ClipHeight - stroke/2
+	wing2ClipX := parenClipX + parenClipWidth - margin
+	wing2CircX := wing2ClipX + margin
 
 	// all
-	widthAll := wing2X + wing2ClipWidth + margin
-	heightAll := oSize*2 + margin*2
+	widthAll := wing2ClipX + wingClipWidth + margin - stroke/2
+	heightAll := wingCircY + oSize/2 + margin //oSize*3 + margin*2
 
 	// header
 	fmt.Println(`<?xml version="1.0" ?>`)
@@ -70,18 +67,18 @@ func main() {
 
 	// wing 1
 	fmt.Println(`  <clipPath id="wing1Clip">`)
-	fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" stroke-width="1"/>`,
-		wing1ClipX, wing1ClipY, wing1ClipWidth, wing1ClipHeight)
+	fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
+		wing1ClipX, wingClipY, wingClipWidth, wingClipHeight)
 	fmt.Println()
 	fmt.Println(`  </clipPath>`)
 	fmt.Printf(`  <circle clip-path="url(#wing1Clip)" cx="%d" cy="%d" r="%d" />`,
-		wing1CircX, wing1CircY, wing1CircR)
+		wing1CircX, wingCircY, wingCircR)
 	fmt.Println()
-	featherR := (wing1CircR - stroke) / featherN / 2
+	featherR := (wingCircR - stroke) / featherN / 2
 	feather0X := wing1ClipX + stroke/2
-	feather0Y := wing1CircY + oSize - featherR
+	feather0Y := wingCircY + oSize/2 - featherR
 	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
-		feather0X, wing1CircY, feather0X, feather0Y)
+		feather0X, wingCircY, feather0X, feather0Y)
 	fmt.Println()
 	for i := 0; i < featherN; i++ {
 		featherX := feather0X + featherR + i*featherR*2
@@ -101,7 +98,7 @@ func main() {
 		fmt.Println()
 		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
 			featherX+featherR,
-			feather0Y-(i+1)*featherR*2-featherR-(featherN-i)*stroke,
+			featherY-featherR-(featherN-i)*(featherR),
 			featherX+featherR,
 			featherY,
 		)
@@ -122,15 +119,6 @@ func main() {
 		parenX, parenY, parenR)
 	fmt.Println()
 
-	// base line
-	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="red" stroke-width="1" />`,
-		margin+oSize,
-		parenClipY+parenClipHeight+1,
-		parenClipX+parenClipWidth,
-		parenClipY+parenClipHeight+1,
-	)
-	fmt.Println()
-
 	// n
 	fmt.Println(`  <clipPath id="nClip">`)
 	fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
@@ -144,25 +132,52 @@ func main() {
 		nCircX, nCircY, nCircR)
 	fmt.Println()
 	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
-		nLine2X, nLineY1, nLine2X, nLineY2)
+		nLine2X, nLine2Y1, nLine2X, nLineY2)
 	fmt.Println()
 
 	// wing 2
 	fmt.Println(`  <clipPath id="wing2Clip">`)
 	fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
-		wing2X, wing2Y, wing2ClipWidth, wing2ClipHeight)
+		wing2ClipX, wingClipY, wingClipWidth, wingClipHeight)
 	fmt.Println()
 	fmt.Println(`  </clipPath>`)
-	fmt.Printf(`  <ellipse clip-path="url(#wing2Clip)" cx="%d" cy="%d" rx="%d" ry="%d" />`,
-		wing2X, wing2Y, wing2ElliRX, wing2ElliRY)
 	fmt.Printf(`  <circle clip-path="url(#wing2Clip)" cx="%d" cy="%d" r="%d" />`,
-		wing2X, wing2Y, wing2CircR)
+		wing2CircX, wingCircY, wingCircR)
 	fmt.Println()
+	featherR = (wingCircR - stroke) / featherN / 2
+	feather0X = wing2ClipX + wingClipWidth - stroke/2
+	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
+		feather0X, wingCircY, feather0X, feather0Y)
+	fmt.Println()
+	for i := 0; i < featherN; i++ {
+		featherX := feather0X - featherR - i*featherR*2
+		featherY := feather0Y - i*featherR*2
+		fmt.Printf(`  <clipPath id="wing2feather%dClip">`, i+1)
+		fmt.Println()
+		fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
+			featherX-featherR-stroke/2,
+			featherY,
+			featherR*2+stroke,
+			featherR+stroke/2,
+		)
+		fmt.Println()
+		fmt.Println(`  </clipPath>`)
+		fmt.Printf(`  <circle clip-path="url(#wing2feather%dClip)" cx="%d" cy="%d" r="%d" />`,
+			i+1, featherX, featherY, featherR)
+		fmt.Println()
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
+			featherX-featherR,
+			featherY-featherR-(featherN-i)*(featherR),
+			//feather0Y-(i+1)*featherR*2-featherR-(featherN-i)*stroke,
+			featherX-featherR,
+			featherY,
+		)
+		fmt.Println()
+	}
 
-	// lowest line
 	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="blue" stroke-width="1" />`,
 		0, heightAll, widthAll, heightAll)
-	fmt.Println()
-
+	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="blue" stroke-width="1" />`,
+		widthAll, 0, widthAll, heightAll)
 	fmt.Println(`</svg>`)
 }
