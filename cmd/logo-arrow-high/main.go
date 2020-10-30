55e9c253f429163b650b0f6ea93374f267e36fb7
Ole Bulbuk <ole@bulbuk.de>
2020-10-30 15:42:48 +0100

    Add new multi color (narrow) logo

---
+++ cmd/logo-arrow-high/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// global
+	oSize := 200
+	color := "black"
+	stroke := oSize / 10
+	margin := stroke/2 + oSize/20
+	featherN := 5
+
+	// O (1)
+	oR := oSize / 4
+	oY := margin + oR
+
+	// ^
+	arrWidth := stroke * 2
+	arrHeadLen := oSize * 3 / 4
+	arrHeadX := margin + stroke/2 + arrHeadLen*2/3
+	arrHeadY := oY + oR + stroke + stroke/2
+
+	// O (2)
+	oX := arrHeadX
+
+	// |
+	arrShaftY := arrHeadY + arrWidth*2 + stroke
+	arrShaftX := arrHeadX - arrWidth/2
+	arrShaftLen := arrHeadLen // * 4 / 3
+
+	// square
+	squareWidth := oSize
+	squareHeight := oSize / 4
+
+	// wing (2)
+	wingShoulder := oSize / 6
+	wingClipWidth := oSize + stroke/2 + wingShoulder
+	wingClipHeight := oSize + stroke/2
+	wingClipY := margin + oSize*3/4
+	wingCircY := wingClipY + wingClipHeight
+	wingCircR := oSize
+	wing2ClipX := arrHeadX + wingClipWidth/2 - wingShoulder
+	wing2CircX := wing2ClipX + wingShoulder
+	featherR := (wingCircR - stroke/2) / featherN / 2
+	feather0X := wing2ClipX + wingClipWidth - stroke/2
+	feather0Y := wingCircY + oSize/2 - featherR
+
+	// all
+	widthAll := wing2ClipX + wingClipWidth + margin - stroke/2
+	heightAll := wingCircY + oSize/2 + margin
+
+	// header
+	fmt.Println(`<?xml version="1.0" ?>`)
+	fmt.Printf(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="%dpx" height="%dpx" stroke="%s" stroke-width="%d" fill="transparent">`,
+		widthAll, heightAll, color, stroke)
+	fmt.Println()
+
+	// O
+	fmt.Printf(`  <circle cx="%d" cy="%d" r="%d" />`, oX, oY, oR)
+	fmt.Println()
+
+	// ^
+	sin45 := 0.5 * math.Sqrt(2.0) // sin(45 degree) == (1/2) * sqrt(2) == cos(45 degree)
+	arrHeadR := arrWidth / 2
+	arrHeadShift := int(math.Round(float64(arrHeadR) * sin45))
+	arrHeadLowShift := pythagorasSmall(arrWidth)
+	arrHeadBigLineWH := pythagorasSmall(arrHeadLen - arrWidth + arrHeadShift)
+	arrHeadSmlLineWH := pythagorasSmall(arrHeadLen - arrWidth - arrWidth/2 + arrHeadShift)
+	fmt.Printf(`<path d="M%d,%d
+		a%d,%d 0 0,1 %d,%d
+		l %d,%d
+		a%d,%d 0 0,1 %d,%d
+		l %d,%d
+		l %d,%d
+		a%d,%d 0 0,1 %d,%d
+		Z" />`,
+		arrHeadX-arrHeadShift, arrHeadY+arrHeadR-arrHeadShift,
+		arrHeadR, arrHeadR, arrHeadShift*2, 0,
+		arrHeadBigLineWH, arrHeadBigLineWH,
+		arrHeadR, arrHeadR, -arrHeadLowShift, arrHeadLowShift,
+		-arrHeadSmlLineWH, -arrHeadSmlLineWH,
+		-arrHeadSmlLineWH, arrHeadSmlLineWH,
+		arrHeadR, arrHeadR, -arrHeadLowShift, -arrHeadLowShift,
+	)
+	fmt.Println()
+
+	// |
+	fmt.Printf(`  <rect x="%d" y="%d" width="%d" height="%d" rx="%d" ry="%d" />`,
+		arrShaftX, arrShaftY, arrWidth, arrShaftLen, arrWidth/2, arrWidth/2)
+	fmt.Println()
+
+	// square
+	fmt.Printf(`  <rect x="%d" y="%d" width="%d" height="%d" />`,
+		arrHeadX-squareWidth/2, feather0Y+featherR-squareHeight, squareWidth, squareHeight)
+	fmt.Println()
+
+	// wing2
+	fmt.Println(`  <clipPath id="wing2Clip">`)
+	fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
+		wing2ClipX, wingClipY, wingClipWidth, wingClipHeight)
+	fmt.Println()
+	fmt.Println(`  </clipPath>`)
+	fmt.Printf(`  <circle clip-path="url(#wing2Clip)" cx="%d" cy="%d" r="%d" />`,
+		wing2CircX, wingCircY, wingCircR)
+	fmt.Println()
+	fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
+		feather0X, wingCircY, feather0X, feather0Y)
+	fmt.Println()
+	for i := 0; i < featherN; i++ {
+		featherX := feather0X - featherR - i*featherR*2
+		featherY := feather0Y - i*featherR*2
+		fmt.Printf(`  <clipPath id="wing2feather%dClip">`, i+1)
+		fmt.Println()
+		fmt.Printf(`    <rect x="%d" y="%d" width="%d" height="%d" />`,
+			featherX-featherR-stroke/2,
+			featherY,
+			featherR*2+stroke,
+			featherR+stroke/2,
+		)
+		fmt.Println()
+		fmt.Println(`  </clipPath>`)
+		fmt.Printf(`  <circle clip-path="url(#wing2feather%dClip)" cx="%d" cy="%d" r="%d" />`,
+			i+1, featherX, featherY, featherR)
+		fmt.Println()
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" />`,
+			featherX-featherR,
+			featherY-featherR-(featherN-i)*(featherR),
+			featherX-featherR,
+			featherY,
+		)
+		fmt.Println()
+	}
+
+	/*
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="blue" stroke-width="1" />`,
+			0, heightAll, widthAll, heightAll)
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="blue" stroke-width="1" />`,
+			widthAll, 0, widthAll, heightAll)
+	*/
+	fmt.Println(`</svg>`)
+}
+
+func pythagorasSmall(c int) int {
+	return int(math.Round(math.Sqrt(float64(c * c / 2))))
+}
---
+++ cmd/logo-arrow-narrow/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	// global
+	oSize := 100
+	red := "#b00b0b"
+	blue := "#00569a"
+	green := "#0aa00a"
+	orange := "#db6200"
+	margin := oSize / 10
+
+	// wing1 (1)
+	wing1X := oSize
+	wingM := oSize / 5
+
+	// O (1)
+	oR := oSize / 4
+	oY := oR
+
+	// wing1 (2)
+	wingY := oY + oR + margin/2
+
+	// ^
+	arrWidth := margin * 2
+	arrHeadLen := oSize * 3 / 4
+	arrHeadX := oSize + arrHeadLen*1/6
+	arrHeadY := oY + oR + margin
+
+	// O (2)
+	oX := arrHeadX
+
+	// |
+	arrShaftY := arrHeadY + arrWidth*2
+	arrShaftX := arrHeadX - arrWidth/2
+	arrShaftLen := arrHeadLen
+
+	// square
+	squareY := arrShaftY + arrShaftLen + margin
+	squareWidth := oSize
+	squareHeight := wingM
+
+	// wing2
+	wing2X := oX + (oX - wing1X)
+
+	// all
+	allWidth := oX * 2
+	allHeight := squareY + squareHeight
+
+	// header
+	fmt.Println(`<?xml version="1.0" standalone="no" ?>`)
+	fmt.Printf(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="%dpx" height="%dpx">`,
+		allWidth, allHeight)
+	fmt.Println()
+
+	// wing1
+	fmt.Printf(`  <path d="M%d,%d
+		Q%d,%d %d,%d
+		T%d,%d
+		T%d,%d
+		T%d,%d
+		T%d,%d
+		Z"
+		fill="%s" />`,
+		wing1X, wingY,
+		wingM*2, wingY, wingM*2, wingM,
+		margin*3/2, wingM*2,
+		wingM/2, squareY-wingM*2,
+		wingM*2, squareY-wingM*2,
+		wing1X-wingM*2, wingY+wingM*2,
+		orange,
+	)
+	fmt.Println()
+
+	// O
+	fmt.Printf(`  <circle cx="%d" cy="%d" r="%d" fill="%s" />`, oX, oY, oR, green)
+	fmt.Println()
+
+	// ^
+	sin45 := 0.5 * math.Sqrt(2.0) // sin(45 degree) == (1/2) * sqrt(2) == cos(45 degree)
+	arrHeadR := arrWidth / 2
+	arrHeadShift := int(math.Round(float64(arrHeadR) * sin45))
+	arrHeadLowShift := pythagorasSmall(arrWidth)
+	arrHeadBigLineWH := pythagorasSmall(arrHeadLen - arrWidth + arrHeadShift)
+	arrHeadSmlLineWH := pythagorasSmall(arrHeadLen - arrWidth - arrWidth/2 + arrHeadShift)
+	fmt.Printf(`  <path d="M%d,%d
+		a%d,%d 0 0,1 %d,%d
+		l %d,%d
+		a%d,%d 0 0,1 %d,%d
+		l %d,%d
+		l %d,%d
+		a%d,%d 0 0,1 %d,%d
+		Z"
+		fill="%s" />`,
+		arrHeadX-arrHeadShift, arrHeadY+arrHeadR-arrHeadShift,
+		arrHeadR, arrHeadR, arrHeadShift*2, 0,
+		arrHeadBigLineWH, arrHeadBigLineWH,
+		arrHeadR, arrHeadR, -arrHeadLowShift, arrHeadLowShift,
+		-arrHeadSmlLineWH, -arrHeadSmlLineWH,
+		-arrHeadSmlLineWH, arrHeadSmlLineWH,
+		arrHeadR, arrHeadR, -arrHeadLowShift, -arrHeadLowShift,
+		blue,
+	)
+	fmt.Println()
+
+	// |
+	fmt.Printf(`  <rect x="%d" y="%d" width="%d" height="%d" rx="%d" ry="%d" fill="%s" />`,
+		arrShaftX, arrShaftY, arrWidth, arrShaftLen, arrWidth/2, arrWidth/2, blue)
+	fmt.Println()
+
+	// square
+	fmt.Printf(`  <rect x="%d" y="%d" width="%d" height="%d" fill="%s" />`,
+		arrHeadX-squareWidth/2, squareY, squareWidth, squareHeight, red)
+	fmt.Println()
+
+	// wing2
+	fmt.Printf(`  <path d="M%d,%d
+		Q%d,%d %d,%d
+		T%d,%d
+		T%d,%d
+		T%d,%d
+		T%d,%d
+		Z"
+		fill="%s" />`,
+		wing2X, wingY,
+		allWidth-wingM*2, wingY, allWidth-wingM*2, wingM,
+		allWidth-margin*3/2, wingM*2,
+		allWidth-wingM/2, squareY-wingM*2,
+		allWidth-wingM*2, squareY-wingM*2,
+		allWidth-wing1X+wingM*2, wingY+wingM*2,
+		orange,
+	)
+	fmt.Println()
+
+	/*
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="red" stroke-width="1" />`,
+			0, allHeight, allWidth, allHeight)
+		fmt.Printf(`  <line x1="%d" y1="%d" x2="%d" y2="%d" stroke="red" stroke-width="1" />`,
+			allWidth, 0, allWidth, allHeight)
+	*/
+	fmt.Println(`</svg>`)
+}
+
+func pythagorasSmall(c int) int {
+	return int(math.Round(math.Sqrt(float64(c * c / 2))))
+}
